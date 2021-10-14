190d4ab3f5c085d101c425720affcfa145ae5e6d
faeriqramley <mohamad.faeriq.ramley@gmail.com>
2021-10-15 01:02:58 +0800

    added dashes so I can see the console better

--- backend/server.js
+++ backend/server.js
@@ -9,15 +9,6 @@ const PORT = process.env.PORT || 5000
 const app = express();
 app.use(express.json())
 
-app.get("/",(req,res)=>{
-    res.json({message:"ok"})
-})
-
-// app.post("/",(req,res)=>{
-//     User.create({username:"John Doe",email:"123@gmail.com",password:"123456"});
-//     res.json({status:"ok",message:"User Created"});
-// })
-
 app.use("/user",userRoutes);
 app.use("/expense",expenseRoutes);
 app.use("/budget",budgetRoutes);
@@ -25,6 +16,6 @@ app.use("/cashFlow",cashFlowRoutes);
 
 syncTables().then(()=>{
     app.listen(PORT, ()=>{
-        console.log(`listening to Port ${PORT}`)
+        console.log(`=====================listening to Port ${PORT}===============================================================`)
     });
 });
