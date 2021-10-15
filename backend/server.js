e7943c99fb8b4dc1620c8beca424f038b9d71560
faeriqramley <mohamad.faeriq.ramley@gmail.com>
2021-10-15 21:11:35 +0800

    refresh token route done

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+
+const {authenticateToken} = require("./middleware/authMiddleware")
+
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
 const expenseRoutes = require("./routes/expenseRoutes");
@@ -6,13 +10,36 @@ const budgetRoutes = require("./routes/budgetRoutes");
 const cashFlowRoutes = require("./routes/cashFlowRoutes");
 const savingGoalRoutes = require("./routes/savingGoalRoutes");
 const calculatedInfoRoutes = require("./routes/calculatedInfoRoutes");
-const {authenticateToken} = require("./middleware/authMiddleware")
 const {syncTables} = require('./models');
 
 const PORT = process.env.PORT || 5000
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/setcookie', (req, res) => {
+    res.cookie(`CookieTokenName`,`encrypted cookie string Value`,{
+        maxAge: 5000 * 10,
+        // expires works the same as the maxAge
+        secure: true,
+        httpOnly: true,
+        sameSite: 'lax'
+    });
+    res.send('Cookie have been saved successfully');
+});
+
+app.get('/getcookie',(req,res)=>{
+    console.log(req.cookies);
+    if(req.cookies["CookieTokenName"]){
+        console.log(req.cookies["CookieTokenName"]);
+        console.log("yes");
+    } else {
+        console.log("no");
+    }
+    res.send(req.cookies);
+})
+
 app.use("/auth",authRoutes);
 app.use("/user",userRoutes);
 app.use("/expense",authenticateToken,expenseRoutes);
