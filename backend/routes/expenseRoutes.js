dff157ad9bda30ba9501f81efd30235dff54fc71
faeriqramley <mohamad.faeriq.ramley@gmail.com>
2021-10-15 19:27:47 +0800

    halfway done with auth. Before using cookieparser

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const {post_addExpense,get_search,get_viewUser} = require("../controllers/expenseController");
-
 router.get("/viewUser/:useruuid",get_viewUser);
 router.get("/search/:useruuid",get_search);
 router.post("/log/:useruuid",post_addExpense);
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,25 @@
 const express = require("express");
-const {syncTables} = require('./models');
 const userRoutes = require("./routes/userRoutes");
+const authRoutes = require("./routes/authRoutes");
 const expenseRoutes = require("./routes/expenseRoutes");
 const budgetRoutes = require("./routes/budgetRoutes");
 const cashFlowRoutes = require("./routes/cashFlowRoutes");
 const savingGoalRoutes = require("./routes/savingGoalRoutes");
 const calculatedInfoRoutes = require("./routes/calculatedInfoRoutes");
+const {authenticateToken} = require("./middleware/authMiddleware")
+const {syncTables} = require('./models');
+
 const PORT = process.env.PORT || 5000
 const app = express();
-app.use(express.json())
 
+app.use(express.json())
+app.use("/auth",authRoutes);
 app.use("/user",userRoutes);
-app.use("/expense",expenseRoutes);
-app.use("/budget",budgetRoutes);
-app.use("/cashFlow",cashFlowRoutes);
-app.use("/savingGoal",savingGoalRoutes);
-app.use("/calculatedInfo",calculatedInfoRoutes);
+app.use("/expense",authenticateToken,expenseRoutes);
+app.use("/budget",authenticateToken,budgetRoutes);
+app.use("/cashFlow",authenticateToken,cashFlowRoutes);
+app.use("/savingGoal",authenticateToken,savingGoalRoutes);
+app.use("/calculatedInfo",authenticateToken,calculatedInfoRoutes);
 
 syncTables().then(()=>{
     app.listen(PORT, ()=>{
