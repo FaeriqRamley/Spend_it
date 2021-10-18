486fc378b0aab733eb440a4375c8fc0c75cdcd3a
faeriqramley <mohamad.faeriq.ramley@gmail.com>
2021-10-18 14:32:46 +0800

    created signup and login page (non functional)

--- frontend/src/app/landing/landing.component.ts
+++ frontend/src/app/landing/landing.component.ts
@@ -6,8 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
-
   constructor() { }
+
+  public userLogin = true;
+  public userName = ''
+  public userEmail = '';
+  public userPassword = '';
+  public userCfmPassword = '';
+  public errMsg = '';
+
+  toggleLoginSignup(event:any){
+    event.preventDefault();
+    this.userLogin = !this.userLogin;
+    this.errMsg = '';
+  }
 
   ngOnInit(): void {
   }
