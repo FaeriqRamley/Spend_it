0a613474e01b8b0dda9d33745ed21711f63bc6b1
faeriqramley <mohamad.faeriq.ramley@gmail.com>
2021-10-20 11:49:52 +0800

    fixed unscrollable background bug

--- frontend/src/app/walletinfo-large/walletinfo-large.component.ts
+++ frontend/src/app/walletinfo-large/walletinfo-large.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import IUserWallet from '../interfaces/userWallet';
+import { WalletInfoService } from '../services/wallet-info.service';
 
 @Component({
   selector: 'app-walletinfo-large',
   templateUrl: './walletinfo-large.component.html',
-  styleUrls: ['./walletinfo-large.component.css']
+  styleUrls: ['../walletinfo-small/walletinfo-small.component.html']
 })
 export class WalletinfoLargeComponent implements OnInit {
 
-  constructor() { }
-
+  public walletInfo:IUserWallet = this.walletInfoService.userWallet
+  private subscription:Subscription;
+  public netWorthClass:string = 'negative-cash';
+  constructor(private walletInfoService:WalletInfoService) {
+    this.subscription = this.walletInfoService.observableWallet
+  }
+  
   ngOnInit(): void {
+    this.subscription = this.walletInfoService.observableWallet
+    .subscribe( (item:any)=>{
+      console.log('networth',item.net_worth);
+      if(item.net_worth > 0){
+        console.log('this is true for',item.net_worth);
+        this.netWorthClass = 'positive-cash';
+      } else{
+        this.netWorthClass = 'negative-cash';
+      }
+
+      this.walletInfo = item
+    })
   }
 
 }
