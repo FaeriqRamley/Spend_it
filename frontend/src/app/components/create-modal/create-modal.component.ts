998557511c44b12447e59c2f97aa964f58103055
faeriqramley <mohamad.faeriq.ramley@gmail.com>
2021-10-20 20:56:38 +0800

    create modal component to create savings done

--- frontend/src/app/components/create-modal/create-modal.component.ts
+++ frontend/src/app/components/create-modal/create-modal.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit, TemplateRef } from '@angular/core';
 import { BsModalService,BsModalRef } from 'ngx-bootstrap/modal';
+import { SavingGoalsService } from 'src/app/services/saving-goals.service';
+import { WalletInfoService } from 'src/app/services/wallet-info.service';
 
 @Component({
   selector: 'app-create-modal',
@@ -8,13 +10,53 @@ import { BsModalService,BsModalRef } from 'ngx-bootstrap/modal';
 })
 export class CreateModalComponent implements OnInit {
 
+  public errMsg:string = '';
+  public title:string = '';
+  public value:number = 1;
   @Input() modalRef?:BsModalRef;
+  
 
-  constructor(private modalService:BsModalService) { }
-
-  openModal(template:TemplateRef<any>){
-    this.modalRef = this.modalService.show(template);
+  constructor(private modalService:BsModalService,private savingGoalsService:SavingGoalsService, private walletInfoService:WalletInfoService) {
   }
+
+  validateSavings(input:any){
+    const currentSavings = this.savingGoalsService.userSavings;
+    let outputMsg = ''
+    if(input.title === ''){
+      outputMsg += 'please input something for the title'
+    }
+    for(const saving of currentSavings){
+      if(input.title === saving.title){
+        outputMsg += ' saving with the same title already exists'
+      }
+    }
+    if(input.target<1){
+      outputMsg += ' value must be at least $1'
+    }
+
+    return outputMsg
+
+  }
+
+  onClickSubmit(event:any){
+    event.preventDefault();
+    console.log('clicked');
+    const newEntry = {
+      title: this.title,
+      target: this.value
+    }
+    this.errMsg = this.validateSavings(newEntry);
+
+    if (this.errMsg !== ""){
+      this.title = "";
+      this.value = 1;
+    } else {
+      this.savingGoalsService.createUserSavings(newEntry);
+      this.modalRef?.hide();
+    }
+
+  }
+
 
   ngOnInit(): void {
   }
--- frontend/src/app/components/saving-goals/saving-goals.component.ts
+++ frontend/src/app/components/saving-goals/saving-goals.component.ts
@@ -12,20 +12,7 @@ export class SavingGoalsComponent implements OnInit {
   public subscription:Subscription;
   modalRef?:BsModalRef;
 
-  public userSavings = [
-    {
-      uuid:'1234-5678-9112',
-      title:'This Goal',
-      target:700,
-      current:87,
-    },
-    {
-      uuid:'1234-5678-9113',
-      title:'That Goal',
-      target:1200,
-      current:957,
-    },
-  ]
+  public userSavings:any = [];
 
   constructor(private modalService:BsModalService,private savingGoalsService:SavingGoalsService) {
     this.subscription = this.savingGoalsService.observableUserSavings;
